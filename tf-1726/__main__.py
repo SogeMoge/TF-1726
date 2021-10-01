d863a35e822dd5e419dc219b2dbcaf02e47b73ce
SogeMoge <valterob@gmail.com>
2021-10-02 00:43:53 +0300

    initial commit with template bot

---
+++ tf-1726/__main__.py
@@ -0,0 +1,18 @@
+import os
+
+import discord
+
+
+bot = discord.Bot()
+discord_token = os.getenv("DISCORD_TOKEN", None)
+
+@bot.slash_command()
+async def hello(ctx, name: str = None):
+    name = name or ctx.author.name
+    await ctx.respond(f"Hello {name}!")
+
+@bot.user_command(name="Say Hello")
+async def hi(ctx, user):
+    await ctx.respond(f"{ctx.author.mention} says hello to {user.name}!")
+
+bot.run(discord_token)
