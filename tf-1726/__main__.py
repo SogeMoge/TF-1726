23a11952605808f8eaadb2e9a6eccecf053b1a26
SogeMoge <valterob@gmail.com>
2021-11-07 21:01:52 +0300

    add slash for embed test

--- tf-1726/__main__.py
+++ tf-1726/__main__.py
@@ -17,4 +17,12 @@ async def hello(ctx):
     """Say hello to the bot"""  # the command description can be supplied as the docstring
     await ctx.send(f"Hello, {ctx.author.display_name}!")
 
+@bot.slash_command(guild_ids=[747905921115619399])  # create a slash command for the supplied guilds
+async def info(ctx):
+    """Useful X-Wing resourses"""  # the command description can be supplied as the docstring
+    embed = discord.Embed(title="X-Wing resources", colour=discord.Colour(0xFFD700))
+    embed.add_field(name="https://www.atomicmassgames.com/xwing-documents", value=pos, inline=False)
+    embed.set_footer(text=ctx.author.name, icon_url = ctx.author.avatar_url)
+    await ctx.send(embed=embed)
+
 bot.run(token)
