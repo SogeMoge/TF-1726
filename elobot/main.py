9e02543323235897173abd7966838573a91099c8
SogeMoge <46896745+SogeMoge@users.noreply.github.com>
2022-10-13 16:06:49 +0300

    Add default embed for yasb embed

--- elobot/main.py
+++ elobot/main.py
@@ -273,13 +273,15 @@ async def on_message(message):
                     colour=discord.Colour.random(),
                     url=message.content,
                     description="YASB Legacy 2.0 list"
-            else :
-                embed = discord.Embed(
-                    title="Infamous squadron",
-                    colour=discord.Colour.random(),
-                    url=message.content,
-                    description="YASB Legacy 2.0 list" 
                 )
+
+        if not embed:
+            embed = discord.Embed(
+                title="Infamous squadron",
+                colour=discord.Colour.random(),
+                url=message.content,
+                description="YASB Legacy 2.0 list"
+            )
 
         embed.set_footer(
             text=message.author.display_name, icon_url=message.author.display_avatar
