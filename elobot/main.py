18032336b86928ccc1b26c7182ac87c75588b81e
SogeMoge <46896745+SogeMoge@users.noreply.github.com>
2022-10-13 15:53:36 +0300

    fix syntax

--- elobot/main.py
+++ elobot/main.py
@@ -273,8 +273,8 @@ async def on_message(message):
                     colour=discord.Colour.random(),
                     url=message.content,
                     description="YASB Legacy 2.0 list"
-            else:
-               embed = discord.Embed(
+            else :
+                embed = discord.Embed(
                     title="Infamous squadron",
                     colour=discord.Colour.random(),
                     url=message.content,
